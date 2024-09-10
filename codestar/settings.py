012fde96b78e13615eb7e3cd106a17c3ce2af40d
Victor Hugo Barili Schons <hugoht3@hotmail.com>
2024-09-10 14:12:42 +0000

    blog models update

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-76c4_^c*8kxp2_^0(r4r6-a$3d)i33-s%zp91eg60$8q76v$t8'
+SECRET_KEY = 'django-insecure-76c4_^c*8kxp2_^0(r4r6-a$3d)i33-s%zp91eg60$8q76v$t8'
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
